dc02b2d4c37a7b56a48be0e689c0785507ea31f3
Krishantchauhan <krashantchauhan@gmail.com>
2023-07-21 23:53:59 +0530

    Completed fully working
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import Header from "./Header";
 import Code from "./Code.jsx";
 
+import Result from "./Result.jsx";
+
 const Home = () => {
   return (
     <>
       <Header />
       <Code />
+      <Result />
     </>
   );
 };
